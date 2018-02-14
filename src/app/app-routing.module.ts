48d38b49e03436bc0a5eb169e7fde3033d18d634
Rajachandar <rajachandar.ntpl@gmail.com>
2018-02-14 12:35:47 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import {AboutComponent } from './about/about.component';
+import {ServicesComponent } from './services/services.component';
+import {CareersComponent } from './careers/careers.component';
+import {ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+ {
+  path:'',
+  component: HomeComponent
+ },
+ {
+  path:'about',
+  component: AboutComponent
+ },
+ {
+  path:'services',
+  component: ServicesComponent
+ },
+ {
+  path:'careers',
+  component: CareersComponent
+ },
+ {
+  path:'contact',
+  component: ContactComponent
+ }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { DataService } from './data.service';
+import{ BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import{ FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
+import { CareersComponent } from './careers/careers.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ServicesComponent,
+    ContactComponent,
+    CareersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
